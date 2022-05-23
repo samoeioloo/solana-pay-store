952232f0ed2eeb8737d7c765c766d009016f8bc7
samoei <samoeiayuka@gmail.com>
2022-05-23 09:21:46 +0200

    set up wallet providers

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,50 @@
 import React, { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"; //enumerable object for the available networks
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"; //component that will prompt the user to select their wallet
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react"; // connection provider :takes an RPC endpoint in and lets us talk directly to the nodes on the Solana blockchain. used to send transactions
+// wallet provider: standard interface for connecting to wallets
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js"; // generates an RPC endpoint for us based on the network we give it
+
+import "@solana/wallet-adapter-react-ui/styles.css";
 import "../styles/globals.css";
 import "../styles/App.css";
 
 const App = ({ Component, pageProps }) => {
+  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
 
   return (
-    <Component {...pageProps} />
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 };
 
