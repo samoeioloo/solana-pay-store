574f45c55f39eb05047526c7fc39f4971c9d2ba9
samoei <samoeiayuka@gmail.com>
2022-05-23 15:21:39 +0200

    added connection to wallets

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,37 @@
 import React from "react";
 import HeadComponent from '../components/Head';
+import { PublicKey } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
-  
+  // (hook) fetches the users' public key (wallet address) from any wallet we support... subscribes to providers in _app.js
+  const { publicKey } = useWallet(); 
+  const renderNotConnectedContainer = () => (
+    <div>
+        <iframe src="https://giphy.com/embed/6CovzgyTig7M4" width="480" height="304" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/magic-wand-hifw-6CovzgyTig7M4">via GIPHY</a></p>
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>    
+    </div>
+  );
   return (
     <div className="App">
-      <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header">  🧙 Ollivanders Wand Store 🪄</p>
+          <p className="header"> 🧙 Ollivanders Wand Store</p>
           <p className="sub-text">Where the wand finds the wizard</p>
         </header>
-
         <main>
-          <iframe src="https://giphy.com/embed/6CovzgyTig7M4" width="480" height="304" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/magic-wand-hifw-6CovzgyTig7M4">via GIPHY</a></p>
+          {/* We only render the connect button if public key doesn't exist */}
+          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
         </main>
-
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
           <a
