8e93538a5e4743555b726d18a675c7e2b1a5c99b
samoei <samoeiayuka@gmail.com>
2022-05-26 08:33:26 +0200

    added background

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import HeadComponent from '../components/Head';
 import { PublicKey } from '@solana/web3.js';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-
+import Product from "../components/Product";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
@@ -11,7 +11,19 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   // (hook) fetches the users' public key (wallet address) from any wallet we support... subscribes to providers in _app.js
-  const { publicKey } = useWallet(); 
+  const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
   const renderNotConnectedContainer = () => (
     <div>
         <iframe src="https://giphy.com/embed/6CovzgyTig7M4" width="480" height="304" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/magic-wand-hifw-6CovzgyTig7M4">via GIPHY</a></p>
@@ -19,6 +31,13 @@ const App = () => {
       <div className="button-container">
         <WalletMultiButton className="cta-button connect-wallet-button" />
       </div>    
+    </div>
+  );
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
     </div>
   );
   return (
@@ -30,7 +49,7 @@ const App = () => {
         </header>
         <main>
           {/* We only render the connect button if public key doesn't exist */}
-          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
